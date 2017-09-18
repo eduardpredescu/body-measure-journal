9a93e86096107da85127adf79a7cc98cea4c55d2
Eduard Predescu <eduard.florin.predescu@gmail.com>
2017-09-18 20:13:05 +0300

    fixed jwt

--- backend/journal/api/utils.py
+++ backend/journal/api/utils.py
@@ -1,22 +1,19 @@
-from django.contrib.auth import get_user_model
 from datetime import datetime
 from rest_framework_jwt.settings import api_settings
 
 
 
 def jwt_payload_handler(user):
-    email_field = username_field = get_user_model().USERNAME_FIELD
     email = user.get_username()
 
     payload = {
         'user_id': user.pk,
-        'email': email,
-        'username': user.username,
+        'username': email,
+        'user': user.username,
         'is_admin': user.is_admin,
         'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
     }
 
-    payload[username_field] = email
 
     # Include original issued at time for a brand new token,
     # to allow token refresh
